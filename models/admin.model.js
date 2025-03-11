93ec00ea7c8c43070ca6afcc724b00e177c6fb5e
lalith kumar <lalithdev123@gmail.com>
2025-03-11 23:42:22 +0530

    updated models

---
+++ models/admin.model.js
@@ -0,0 +1,22 @@
+import { Schema,model } from "mongoose";
+
+const adminSchema=new Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    fullname:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6,
+    }
+
+},{timestamps:true});
+const Admin=model("Admin",adminSchema);
+
+export default Admin;
---
+++ models/batches.model.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema,model } from "mongoose";
+
+const batchesSchema=new Schema({
+    batch_name:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    students:[{studnet_id:mongoose.Schema.Types.ObjectId,ref:"Students"}],
+    
+},{timestamps:true})
+
