737096f4a050281e7fc740ace3b942c17fcbb11f
ganesh-basavoju <basavojuganesh@gmail.com>
2025-03-26 23:28:12 +0530

    finnaly completed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import batchesRoutes from "./routes/batches.route.js";
 
 dotenv.config();
 
-const PORT = 5001;
+const PORT = process.env.PORT||5001;
 app.use(express.json());
 app.use(cookieParser());
 
